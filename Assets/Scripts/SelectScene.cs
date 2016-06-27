77491dc2bf02518278cf074a41c59fa433daad71
NeoSouldier <FoxBookAir@FoxBookAir.local>
2016-06-27 21:46:51 +0100

    Added Scene switching functionality to the Feed Items on MouseDown()

---
+++ Assets/Scripts/SelectScene.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SelectScene : MonoBehaviour 
+{	
+    public string m_sceneName = "";   
+
+    void OnMouseDown()
+    {
+        SceneManager.LoadScene(m_sceneName);
+    }
+
+    /*
+    void Update () 
+    {        
+        if (SelectedObject())
+        {
+            SceneManager.LoadScene(m_sceneName);
+        }
+
+    }
+
+    bool SelectedObject()
+    {
+        
+        if ( Input.GetMouseButtonDown(0))
+        {
+            var hit : RaycastHit;
+            var ray : Ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+            if (Physics.Raycast (ray, hit, 100.0))
+            {  
+                return true;
+            }
+        }       
+
+        return false;
+    }
+    */
+
+}
