f2409b844df6394551839a450fd716fa002e8128
NeoSouldier <FoxBookAir@FoxBookAir.local>
2016-10-07 15:52:18 +0100

    Made project runnable as an App on my S7 Edge device in VR mode
    This required changing some settings in the Unity project, creating an
    OSIG file (oculus signature) and then Switching Platform.
    
    Following links were useful:
    https://docs.unity3d.com/Manual/VROverview.html
    
    https://developer3.oculus.com/documentation/game-engines/latest/concepts
    /unity-build-android/
    
    Platform switching took forever and I had to leave it on over night, so
    I also created a copy of the Library folder so it wouldn’t take so long
    if I wanted to switch platforms next time.

---
+++ Assets/Scripts/SelectVR.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.VR;
+using System.Collections;
+
+public class SelectVR : MonoBehaviour 
+{
+    void Awake()
+    {
+        UnityEngine.VR.VRSettings.enabled = false;
+    }
+
+    void OnMouseDown()
+    {       
+        VRSettings.enabled = !VRSettings.enabled;
+    }
+}
