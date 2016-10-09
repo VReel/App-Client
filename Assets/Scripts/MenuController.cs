293adfa434d36e33a04dee737be19bb7c6cb0c81
NeoSouldier <FoxBookAir@FoxBookAir.home>
2016-10-09 12:31:31 +0100

    Added the Sphere Menu
    I now have the option of having both the quad and sphere menus.
    
    The quad menu is more traditional and simply stretches the panoramic
    images onto a quad, but the sphere menu shows the 360 image that you
    are going to go into when you pick it.
    
    I think I will do a little effect later where the sphere you pick grows
    and consumes you, so that you understand that you are going into the
    image!

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -3,15 +3,23 @@ using System.Collections;
 
 public class MenuController : MonoBehaviour 
 {
-    public GameObject m_menu = null;
+    public GameObject m_quadMenu = null;
+    public GameObject m_sphereMenu = null;
 
 	void Update () 
     {
-        if (m_menu != null && Input.GetKeyDown(KeyCode.M))
+        if (m_quadMenu != null && Input.GetKeyDown(KeyCode.N))
         {
-            bool active = m_menu.activeInHierarchy;
+            bool active = m_quadMenu.activeInHierarchy;
 
-            m_menu.SetActive(!active);
+            m_quadMenu.SetActive(!active);
+        }
+
+        if (m_sphereMenu != null && Input.GetKeyDown(KeyCode.M))
+        {
+            bool active = m_sphereMenu.activeInHierarchy;
+
+            m_sphereMenu.SetActive(!active);
         }
 	}
 }
