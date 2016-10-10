bed22808bce7a6d40c46123044b02d1603c1e587
NeoSouldier <FoxBookAir@FoxBookAir.local>
2016-10-10 17:26:22 +0100

    Added functionality to choose images with Gear VR
    Grabbed a set of scripts from OSVR for interactivity and used them to
    replicate the functionality that I’d built on the computer, to work in
    VR.
    
    This now means that when you run the app through the GearVR you can tap
    on the side of the headset and pick one of the image sphere’s to change
    the Skybox!
    
    This has made it apparent that I need a reticle, and I’d still ideally
    like to be able to switch into VR mode when I want to, rather than have
    to start in it by default!

---
+++ Assets/Standard Assets/VR/MenuButton.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using UnityEngine;
+using VRStandardAssets.Utils;
+
+namespace VRStandardAssets.Menu
+{
+    // This script is for loading scenes from the main menu.
+    // Each 'button' will be a rendering showing the scene
+    // that will be loaded and use the SelectionRadial.
+    public class MenuButton : MonoBehaviour
+    {
+        public event Action<MenuButton> OnButtonSelected;                   // This event is triggered when the selection of the button has finished.
+
+        [SerializeField] private Material m_image;
+        [SerializeField] private VRInteractiveItem m_InteractiveItem;       // The interactive item for where the user should click to load the level.
+
+        private bool m_GazeOver;                                            // Whether the user is looking at the VRInteractiveItem currently.
+
+        public bool GetGazeOver()
+        {
+            return m_GazeOver;
+        }
+
+        private void OnEnable ()
+        {
+            m_InteractiveItem.OnOver += HandleOver;
+            m_InteractiveItem.OnOut += HandleOut;
+            m_InteractiveItem.OnClick += HandleClick;
+        }
+
+        private void OnDisable ()
+        {
+            m_InteractiveItem.OnOver -= HandleOver;
+            m_InteractiveItem.OnOut -= HandleOut;
+            m_InteractiveItem.OnClick -= HandleClick;
+        }
+
+        private void HandleOver()
+        {
+            m_GazeOver = true;
+        }
+            
+        private void HandleOut()
+        {
+            m_GazeOver = false;
+        }
+
+        private void HandleClick()
+        {    
+            if (m_image != null)
+            {
+                RenderSettings.skybox = m_image;
+                OnButtonSelected(this);
+            }
+        }
+    }
+}   
