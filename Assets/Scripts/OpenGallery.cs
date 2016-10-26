1a301de263238bcfd7c3bfee0f7a2fdb2cce624c
NeoSouldier <FoxBookAir@FoxBookAir.local>
2016-10-26 13:10:05 +0100

    Got the code to open a file from the SD Card working!
    StackOverflow to the rescue! I was trying to use the WWW class as I’d
    seen examples of people using it, but I posted a question and a guy
    called Kardux showed me that I could just use
    System.IO.File.ReadAllBytes(), combined with texture.LoadImage() to
    grab a picture off my Android device’s SD Card and set it on one of my
    ImageSpheres.
    
    Currently I’ve hardcoded a single path to a file that I already know
    exists. Now I need to figure out how to actually iterate over the
    images without knowing that they exist! Then I’ll also need to check
    whether they are actually 360 images or not!

--- Assets/Scripts/OpenGallery.cs
+++ Assets/Scripts/OpenGallery.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.IO;
 
 public class OpenGallery : MonoBehaviour 
 {
@@ -26,64 +27,41 @@ public class OpenGallery : MonoBehaviour
         OpenAndroidGallery();
     }        
 
+    /*
+     * EXAMPLE OF USING INTENTS!
+     
+    // Taken from http://answers.unity3d.com/questions/537476/open-gallery-android.html
+
+    // Intent intent = new Intent();
+    AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
+    AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+
+    // intent.setAction(Intent.ACTION_VIEW);
+    intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_VIEW"));
+
+    // intent.setData(Uri.parse("content://media/internal/images/media"));
+    AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
+    AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "content://media/internal/images/media"); //parse of the url's file
+    intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject); //call putExtra with the uri object of the file
+    intentObject.Call<AndroidJavaObject>("setType", "image/jpeg"); //set the type of file
+
+    // startActivity(intent);
+    AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+    AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
+    currentActivity.Call("startActivity", intentObject);
+    */
+
     public void OpenAndroidGallery()
     {
-/*
-        // Taken from http://answers.unity3d.com/questions/537476/open-gallery-android.html
+        GameObject imageSphere = (GameObject) Instantiate(m_imageSphere, new Vector3(-0.5f, 0.0f, 3.0f), Quaternion.identity);       
+        Texture2D texture = new Texture2D(2, 2, TextureFormat.ARGB32, false);
 
-        // Intent intent = new Intent();
-        AndroidJavaClass intentClass = new AndroidJavaClass("android.content.Intent");
-        AndroidJavaObject intentObject = new AndroidJavaObject("android.content.Intent");
+        string path = "/storage/emulated/0/DCIM/Gear 360/SAM_100_0091.jpg"; // Hardcoded path
+        byte[] fileByteData = File.ReadAllBytes(path); // make sure to have Write Access: External (SDCard)
+        texture.LoadImage(fileByteData);
 
-        // intent.setAction(Intent.ACTION_VIEW);
-        intentObject.Call<AndroidJavaObject>("setAction", intentClass.GetStatic<string>("ACTION_VIEW"));
-
-        // intent.setData(Uri.parse("content://media/internal/images/media"));
-        AndroidJavaClass uriClass = new AndroidJavaClass("android.net.Uri");
-        AndroidJavaObject uriObject = uriClass.CallStatic<AndroidJavaObject>("parse", "content://media/internal/images/media"); //parse of the url's file
-        intentObject.Call<AndroidJavaObject>("putExtra", intentClass.GetStatic<string>("EXTRA_STREAM"), uriObject); //call putExtra with the uri object of the file
-        intentObject.Call<AndroidJavaObject>("setType", "image/jpeg"); //set the type of file
-
-        // startActivity(intent);
-        AndroidJavaClass unity = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-        AndroidJavaObject currentActivity = unity.GetStatic<AndroidJavaObject>("currentActivity");
-        currentActivity.Call("startActivity", intentObject);
-*/
-
-        StartCoroutine(CreateSphereAndLoadTexture_1());
+        imageSphere.GetComponent<MeshRenderer>().material.mainTexture = texture;
 
         Debug.Log("------- VREEL: Open Gallery called");
-
-    }
-
-    IEnumerator CreateSphereAndLoadTexture_1()
-    {
-        //DirectoryInfo dir = new DirectoryInfo(Application.persistentDataPath);
-        //FileInfo[] images = dir.GetFiles("*.png");
-
-        //FileInfo firstFileInfo = images[0];
-        //foreach (FileInfo f in images) 
-
-        GameObject imageSphere = (GameObject) Instantiate(m_imageSphere, new Vector3(-1.5f, 0.0f, 3.0f), Quaternion.identity);
-        Debug.Log("------- VREEL: Instantiated image sphere");
-
-        //string path = "/data/media/0/DCIM/Gear 360/SAM_100_0002.jpg";
-        string path = "/storage/emulated/0/DCIM/Camera/20161019_142127.jpg";
-        WWW www = new WWW("file://" + path);
-        Debug.Log("------- VREEL: Attempting to open hardcoded file://" + path);
-
-        yield return www;
-        Debug.Log("------- VREEL: Yield returned");
-
-        if (string.IsNullOrEmpty(www.error))
-        {            
-            imageSphere.GetComponent<Material>().mainTexture = www.texture;
-            Debug.Log("------- VREEL: No Error, texture changed successfully");
-        }
-        else
-        {
-            Debug.Log("------- VREEL: Error returned = " + www.error);
-        }
-
     }
 }
