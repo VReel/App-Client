e0014a1ec0c25d3bc3cd1720128b2a1768f10acf
Arthur Fox <vreel@vreel.io>
2017-04-13 21:20:27 +0100

    First Commit into Like branch - Created an empty Like file

---
+++ Assets/Scripts/Systems/Like.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Like : MonoBehaviour 
+{   
+    // **************************
+    // Member Variables
+    // **************************
+
+    [SerializeField] private AppDirector m_appDirector;
+    [SerializeField] private User m_user;
+
+    private BackEndAPI m_backEndAPI;
+    private CoroutineQueue m_coroutineQueue;
+
+    // **************************
+    // Public functions
+    // **************************
+
+    public void Start() 
+	{
+        m_coroutineQueue = new CoroutineQueue(this);
+        m_coroutineQueue.StartLoop();
+
+        m_backEndAPI = new BackEndAPI(this, m_user.GetErrorMessage(), m_user);
+	}              
+     
+    // **************************
+    // Private/Helper functions
+    // **************************
+
+
+}
