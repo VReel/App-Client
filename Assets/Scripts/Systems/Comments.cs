a6c4af8f35accebed765b0390edf241bc8628f4b
Arthur Fox <vreel@vreel.io>
2017-04-26 16:49:02 +0100

    Created new branch called "Comments"
    This commit simply sets BackEnd Environment back to Development and
    creates a new “Comments” component.
    
    This branch will work on getting commenting functionally finished.
    Allowing users to:
    (1) View all comments on a post
    (2) Edit their comments on a post
    (3) Add new comments
    (4) Select Hashtags within a comment and be taken to the appropriate
    Search tab (this is the most ambitious piece of functionality)

---
+++ Assets/Scripts/Systems/Comments.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class Comments : MonoBehaviour
+{
+    // **************************
+    // Member Variables
+    // **************************
+
+    // **************************
+    // Public functions
+    // **************************
+
+    public void Start()
+    {
+    }
+
+    // **************************
+    // Private/Helper functions
+    // **************************
+
+}
