a59978cb690435d0fa03d6921ce200e28556a7b0
Arthur Fox <vreel@vreel.io>
2017-04-29 14:37:32 +0100

    Added a slight floaty Animation to ImageSpheres
    This is just a nice touch that I thought I’d add that I can remove at
    any time if we decide we don’t like it.
    
    Now ImageSphere’s float a little around their centre of mass, creating
    a floaty feeling to them. This is simply done by adding a small
    variation every frame to the transform’s x,y,z position based on a sine
    wave, this is done in the ImageSphereAnimation component!

--- Assets/Scripts/UI/ImageSphereAnimation.cs
+++ Assets/Scripts/UI/ImageSphereAnimation.cs
@@ -7,15 +7,61 @@ public class ImageSphereAnimation : MonoBehaviour
     // Member Variables
     // **************************
 
-    //[SerializeField] private Image m_followButton;
+    [SerializeField] private float m_timeTakenForFullSineWave = 10; // x second for full sign rotation
+    [SerializeField] private float m_magnitude = 0.015f; // magnitude of variance
+    [SerializeField] private float m_ChanceOfSignFlip = 0.001f; // magnitude of randomVariance
 
+    private const float kLengthOfSine = 2 * Mathf.PI;
+
+    private Vector3 m_originalPos;
+    private Vector3 m_currPos;
+    private Vector3 m_currTimeVec;
+    private Vector3 m_currTimeVecSign;
 
     // **************************
     // Public functions
     // **************************
 
-    public void Update()
+    public void Start()
     {
-        // Update transfrom.x = original.x + sin(t*speed) * magnitude
+        m_originalPos = transform.localPosition;
+        m_currPos = m_originalPos;
+        m_currTimeVec = new Vector3( Random.Range(0.0f, kLengthOfSine), Random.Range(0.0f, kLengthOfSine), Random.Range(0.0f, kLengthOfSine));
+        m_currTimeVecSign = new Vector3( RandomSign(), RandomSign(), RandomSign());
+    }
+
+    public void Update()
+    {                   
+        float updateBasedOnFullTimeTaken = kLengthOfSine/m_timeTakenForFullSineWave;
+
+        m_currTimeVec.x = (m_currTimeVec.x + m_currTimeVecSign.x *
+            ((Time.fixedDeltaTime * updateBasedOnFullTimeTaken))) // + Random.Range(0.0f, updateBasedOnFullTimeTaken) * m_randomVariation)) 
+            % kLengthOfSine;
+        m_currTimeVec.y = (m_currTimeVec.y + m_currTimeVecSign.y *
+                ((Time.fixedDeltaTime * updateBasedOnFullTimeTaken))) // + Random.Range(0.0f, updateBasedOnFullTimeTaken) * m_randomVariation)) 
+            % kLengthOfSine;
+        m_currTimeVec.z = (m_currTimeVec.z + m_currTimeVecSign.z *
+                    ((Time.fixedDeltaTime * updateBasedOnFullTimeTaken)))// + Random.Range(0.0f, updateBasedOnFullTimeTaken) * m_randomVariation)) 
+            % kLengthOfSine;
+
+        if (Random.value < m_ChanceOfSignFlip) m_currTimeVecSign.x *= -1;
+        if (Random.value < m_ChanceOfSignFlip) m_currTimeVecSign.y *= -1;
+        if (Random.value < m_ChanceOfSignFlip) m_currTimeVecSign.z *= -1;
+
+        m_currPos.x = m_originalPos.x + (Mathf.Sin(m_currTimeVec.x) * m_magnitude);
+        m_currPos.y = m_originalPos.y + (Mathf.Sin(m_currTimeVec.y) * m_magnitude);
+        m_currPos.z = m_originalPos.z + (Mathf.Sin(m_currTimeVec.z) * m_magnitude);
+        transform.localPosition = m_currPos;
+
+        if (Debug.isDebugBuild) Debug.Log("------- VREEL: Transform LocalPos: " + m_currPos.ToString("F5") + ", CurrTimeVector: " + m_currTimeVec);
     }       
+
+    // **************************
+    // Private/Helper functions
+    // **************************
+
+    private int RandomSign() 
+    {
+        return Random.value < 0.5f? 1 : -1;
+    }
 }
