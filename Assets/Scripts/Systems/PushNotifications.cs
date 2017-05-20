be42ca45f6cad3bfe883ad50f6cb0da260de8c89
Arthur Fox <vreel@vreel.io>
2017-05-20 01:49:11 +0100

    Added OneSignal to app for PushNotifications!
    This was a hell of a lot easier from the client side than I imagined,
    OneSignal really makes it super-easy!
    
    Now I just need to get the correct Logo for the PushNotifications (as
    I’ve put a temporary one) and get Dan to set it up such that when the
    user has (1) new people following him, or (2) new posts from followers,
    he will actually receive a notification.
    
    I also need to add the option to disable push-notifications for the app
    as that’s also crucial.

---
+++ Assets/Scripts/Systems/PushNotifications.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class PushNotifications : MonoBehaviour 
+{   
+    // **************************
+    // Member Variables
+    // **************************
+
+    // **************************
+    // Public functions
+    // **************************
+
+    public void Start () 
+    {
+        // Enable line below to enable logging if you are having issues setting up OneSignal. (logLevel, visualLogLevel)
+        // OneSignal.SetLogLevel(OneSignal.LOG_LEVEL.INFO, OneSignal.LOG_LEVEL.INFO);
+
+        OneSignal.StartInit("764072f7-5054-4058-b5a6-f5bb724fead1")
+            .HandleNotificationOpened(HandleNotificationOpened)
+            .EndInit();
+
+        // Call syncHashedEmail anywhere in your app if you have the user's email.
+        // This improves the effectiveness of OneSignal's "best-time" notification scheduling feature.
+        // OneSignal.syncHashedEmail(userEmail);
+
+        if (Debug.isDebugBuild)
+        {
+            OneSignal.SetLogLevel(OneSignal.LOG_LEVEL.DEBUG, OneSignal.LOG_LEVEL.DEBUG);
+        }
+    }
+
+    // **************************
+    // Private/Helper functions
+    // **************************
+
+    // Gets called when the player opens the notification.
+    private static void HandleNotificationOpened(OSNotificationOpenedResult result) 
+    {
+    }
+}
