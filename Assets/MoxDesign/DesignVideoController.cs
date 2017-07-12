afb9a8e053e2fc22c86072a19b9613c2d553ad5c
Arthur Fox <vreel@vreel.io>
2017-07-12 14:58:04 +0100

    Created DesignVideoController
    In order for Mox to be able to author the Design Video, we've created a little script that triggers animation events when the internal timer has ticked past the time he's associated with that trigger!

---
+++ Assets/MoxDesign/DesignVideoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesignVideoController : MonoBehaviour 
+{
+    // **************************
+    // Member Variables
+    // **************************
+
+    [SerializeField] private List<TriggerAndTimeEvent> m_events; 
+
+    [System.Serializable]
+    public class TriggerAndTimeEvent
+    {
+        public Animator animator;
+        public string triggerName;
+        public float triggerTime;
+        public bool triggered { get; set; }
+    }
+
+	private float m_currTime = 0;
+
+    // **************************
+    // Public functions
+    // **************************
+
+	public void Update()
+	{
+        m_currTime += Time.fixedDeltaTime;
+
+        UpdateAllEventsBasedOnCurrTime();
+	}
+
+    // **************************
+    // Private functions
+    // **************************
+
+    private void UpdateAllEventsBasedOnCurrTime()
+	{
+        for (int i = 0; i < m_events.Count; i++) 
+		{
+            if (m_events[i].triggered != true) 
+			{			
+                if (m_currTime > m_events[i].triggerTime && m_events[i].animator != null)
+                {
+                    m_events[i].animator.SetBool(m_events[i].triggerName, true);
+                    m_events[i].triggered = true;
+                }
+			}
+		}
+	}
+}
